a250ff0d7273df9750c653cb208db63a91e7ccfe
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2015-10-11 09:04:35 -0400

    Update bra-knn3.R

--- bra-knn3.R
+++ bra-knn3.R
@@ -50,18 +50,61 @@ data.test <- data[-trainIndex, 1:11]
 data.train.size <- data[trainIndex, "size"]
 data.test.size <- data[-trainIndex, "size"]
 
+recommended_size <- function(out, umbral, max_output){
+  col_sizes = colnames(out)
+  values_out <- numeric(nrow(out))
+  sizes_out <- numeric(nrow(out))
+  
+  for(i in 1:nrow(out)) {
+    row <- out[i,]
+    main_values = rev(sort(row))
+    
+    size_out = ''
+    value_out = ''
+    
+    for(k in 1:max_output){
+      value <- main_values[k]
+      if(value > umbral){
+        for(j in 1:length(row)){
+          if((row[j] == value) &!(length(grep(col_sizes[j], size_out)) > 0) ){
+            size_out = paste(size_out, col_sizes[j]) 
+            value_out = paste(value_out, round(value,2))
+            break
+          }
+        }
+      }
+    }
+    sizes_out[i] = size_out
+  }
+  sizes_out
+}
+
+matrix_result <- function(out, winners){
+  result = winners
+  for( i in 1:length(winners)){
+    l = unlist(strsplit(out[i], split=' '))
+    sizes = l[2:length(l)]
+    if(!(winners[i] %in% sizes)){
+      result[i] = 'wrong'
+    } 
+  }
+  return (result)
+}
+
+winners <- as.character(data.test.size)
+
 knnFit <- knn3(data.train, as.factor(data.train.size), k = 11)
+pred <- predict(knnFit,data.test, type = "prob")
 
-pred <- predict(knnFit, data.test, type = "class")
+out <- recommended_size(pred, 0.01,4)
 
-match = (data.test.size == pred)
-success = length(match[match==TRUE])
-success_percent = round(success/length(match) * 100, 2)
+print('recommended_size')
+print(out)
 
-print('SIZE ANALYSIS KNN3:')
-print ( 'Success:')
-print ( success )
-print ( paste(success_percent, "%", sep="")  )
+out2 <- matrix_result(out, winners )
 
-print ( 'Fails:'  )
-print ( paste(100 - success_percent, "%", sep="")  )
+print('matrix_result')
+print(out2)
+
+print('evaluation')
+print(postResample(data.test.size, as.factor(out2)))
