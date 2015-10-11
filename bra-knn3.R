f52b18719c985869e72b38bb8e636dbcac9adb92
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2015-10-11 09:01:41 -0400

    Update bra-knn3.R

--- bra-knn3.R
+++ bra-knn3.R
@@ -24,6 +24,32 @@ data.test <- data[-trainIndex, 1:11]
 data.train.size <- data[trainIndex, "size"]
 data.test.size <- data[-trainIndex, "size"]
 
+library(caret)
+library(sinkr)
+
+styles <- c('2941','3086','3281','3282','3646','3954','852189','N6101','N730023','P011', 'P012','P013','P029')
+style <- 'P029'
+
+data = read.csv("measurement_vectors.csv")
+
+cmd <- "table(data$style)" 
+print(cmd)
+eval(parse(text=cmd))
+
+style <- 'P029'
+data = data[data$style == style,]
+
+data.trans <- eof(data[1:11])
+data[1:11] <- data.trans$A
+
+trainIndex <- createDataPartition(data$size, p = .8, list = FALSE, times = 1)
+
+data.train <- data[trainIndex, 1:11]
+data.test <- data[-trainIndex, 1:11]
+
+data.train.size <- data[trainIndex, "size"]
+data.test.size <- data[-trainIndex, "size"]
+
 knnFit <- knn3(data.train, as.factor(data.train.size), k = 11)
 
 pred <- predict(knnFit, data.test, type = "class")
