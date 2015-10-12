d59e31a1d2c336b1e9c7ed814857147a6ec47848
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2015-10-12 17:28:04 -0400

    Update bra.R

--- bra.R
+++ bra.R
@@ -19,35 +19,33 @@ filled_empty <- eof(data[1:11])
 data[1:11] <- filled_empty$A
 data.active = data[1:11]
 
-
 recommended_size <- function(out, umbral, max_output){
   col_sizes = colnames(out)
-  values_out <- numeric(nrow(out))
   sizes_out <- numeric(nrow(out))
+  size_count_out <- numeric(nrow(out))
+  prob_out <- numeric(nrow(out))
   
   for(i in 1:nrow(out)) {
     row <- out[i,]
-    main_values = rev(sort(row))
-    
+    sorted_values = sort.int(row, decreasing=TRUE, index.return=TRUE)
     size_out = ''
-    value_out = 0
-    
+    size_count = 0
+    prob = 0
     for(k in 1:max_output){
-      value <- main_values[k]
-      if(value > umbral){
-        for(j in 1:length(row)){
-          if((row[j] == value) &!(length(grep(col_sizes[j], size_out)) > 0) ){
-            size_out = paste(size_out, col_sizes[j]) 
-            value_out = value_out + value
-            break
-          }
-        }
+      value <- sorted_values$x[k]
+      if (value < umbral) {
+        break
       }
+      size_out = paste(size_out, col_sizes[sorted_values$ix[k]])
+      prob = prob + value
+      size_count = k
     }
-    round_value_out <- as.character(round(value_out,2))
-    sizes_out[i] = paste(round_value_out, size_out)
+    sizes_out[i] = size_out
+    size_count_out[i] = size_count
+    prob_out[i] = prob
   }
-  sizes_out
+  out = list("sizes"=sizes_out, "counts"=size_count_out, "prob"=prob_out)
+  out
 }
 
 matrix_result <- function(out, winners){
@@ -62,29 +60,27 @@ matrix_result <- function(out, winners){
   return (result)
 }
 
-count <- function(x) {
-  value <- length(unlist(strsplit(x, ' '))) - 2
-  if(value < 0) value = 0
-  return (value)
-}
+
 customSummary <- function(data, lev = NULL, model = NULL){
   if(is.character(data$obs)) data$obs <- factor(data$obs, levels = lev)
   probs <- as.matrix(data[, lev, drop = FALSE])
   winners <- as.character(data[,"obs"])
   out <- recommended_size(probs, 0.25,4)
   
-  print('winners')
-  print(winners)
+  print('recommended_size')
+  print(out$sizes)
+  
   print('recommended_size: ')
   print(out)
-  out2 <- matrix_result(out, winners )
+  
+  out2 <- matrix_result(out$sizes, winners)
+
+  print('matrix_result')
   print(out2)
+
   stats <- postResample(winners, as.factor(out2))
   
-  quantity <- sapply(out, function(x) count(x) )
-  probabylities <- sapply(out, function(x) (as.numeric(unlist(strsplit(x, ' ')))[1]) )
-  
-  stats <- c(stats, mean(quantity), mean(probabylities) )
+  stats <- c(stats, mean(out$counts), mean(out$prob) )
   return (stats)
 }
 
