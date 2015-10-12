42fc37e47e0c9c38d794832cf5b1911739c56d05
Glen Dudek <glen.dudek@gmail.com>
2015-10-12 12:52:02 -0400

    Remove redundant lines added to bra-knn3.R

--- bra-knn3.R
+++ bra-knn3.R
@@ -24,32 +24,6 @@ data.test <- data[-trainIndex, 1:11]
 data.train.size <- data[trainIndex, "size"]
 data.test.size <- data[-trainIndex, "size"]
 
-library(caret)
-library(sinkr)
-
-styles <- c('2941','3086','3281','3282','3646','3954','852189','N6101','N730023','P011', 'P012','P013','P029')
-style <- 'P029'
-
-data = read.csv("measurement_vectors.csv")
-
-cmd <- "table(data$style)" 
-print(cmd)
-eval(parse(text=cmd))
-
-style <- 'P029'
-data = data[data$style == style,]
-
-data.trans <- eof(data[1:11])
-data[1:11] <- data.trans$A
-
-trainIndex <- createDataPartition(data$size, p = .8, list = FALSE, times = 1)
-
-data.train <- data[trainIndex, 1:11]
-data.test <- data[-trainIndex, 1:11]
-
-data.train.size <- data[trainIndex, "size"]
-data.test.size <- data[-trainIndex, "size"]
-
 recommended_size <- function(out, umbral, max_output){
   col_sizes = colnames(out)
   values_out <- numeric(nrow(out))
