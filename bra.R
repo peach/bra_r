5a57a3c9410f014d1e11e0aeda6ae2c4d66c114c
Miguel Sancho Fernandez <sanchocuba@gmail.com>
2015-10-12 15:08:40 -0400

    Update bra.R

--- bra.R
+++ bra.R
@@ -4,7 +4,7 @@ library(pROC)
 
 data = read.csv("~/Documents/dev/peach/r/measurement_vectors.csv")
 
-set.seed(808)
+#set.seed(808)
 
 print('## table(data$style) ##')
 print(table(data$style))
@@ -19,6 +19,7 @@ filled_empty <- eof(data[1:11])
 data[1:11] <- filled_empty$A
 data.active = data[1:11]
 
+
 recommended_size <- function(out, umbral, max_output){
   col_sizes = colnames(out)
   values_out <- numeric(nrow(out))
@@ -29,7 +30,7 @@ recommended_size <- function(out, umbral, max_output){
     main_values = rev(sort(row))
     
     size_out = ''
-    value_out = ''
+    value_out = 0
     
     for(k in 1:max_output){
       value <- main_values[k]
@@ -37,13 +38,14 @@ recommended_size <- function(out, umbral, max_output){
         for(j in 1:length(row)){
           if((row[j] == value) &!(length(grep(col_sizes[j], size_out)) > 0) ){
             size_out = paste(size_out, col_sizes[j]) 
-            value_out = paste(value_out, round(value,2))
+            value_out = value_out + value
             break
           }
         }
       }
     }
-    sizes_out[i] = size_out
+    round_value_out <- as.character(round(value_out,2))
+    sizes_out[i] = paste(round_value_out, size_out)
   }
   sizes_out
 }
@@ -60,11 +62,16 @@ matrix_result <- function(out, winners){
   return (result)
 }
 
+count <- function(x) {
+  value <- length(unlist(strsplit(x, ' '))) - 2
+  if(value < 0) value = 0
+  return (value)
+}
 customSummary <- function(data, lev = NULL, model = NULL){
   if(is.character(data$obs)) data$obs <- factor(data$obs, levels = lev)
   probs <- as.matrix(data[, lev, drop = FALSE])
   winners <- as.character(data[,"obs"])
-  out <- recommended_size(probs, 0.01,4)
+  out <- recommended_size(probs, 0.25,4)
   
   print('winners')
   print(winners)
@@ -72,12 +79,16 @@ customSummary <- function(data, lev = NULL, model = NULL){
   print(out)
   out2 <- matrix_result(out, winners )
   print(out2)
-  postResample(winners, as.factor(out2))
-
+  stats <- postResample(winners, as.factor(out2))
+  
+  quantity <- sapply(out, function(x) count(x) )
+  probabylities <- sapply(out, function(x) (as.numeric(unlist(strsplit(x, ' ')))[1]) )
+  
+  stats <- c(stats, mean(quantity), mean(probabylities) )
+  return (stats)
 }
 
 ###########  SIZE ANALYSIS #############
 y2 <- data$size # target / response
 ctrl <- trainControl(method="repeatedcv", number=10, repeats=3,  classProbs = TRUE, summaryFunction = customSummary) 
 fit <- train(data.active, as.factor(y2), method='knn',trControl=ctrl,  tuneGrid=data.frame(k=3:9))  
-
