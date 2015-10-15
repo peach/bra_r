d56ceddd4a332c4b4c9da1644394b131b2331d82
Glen Dudek <glen.dudek@gmail.com>
2015-10-15 12:57:52 -0400

    misc update to bra-multinom

--- bra-multinom.R
+++ bra-multinom.R
@@ -6,10 +6,8 @@ source("bra-lib.R")
 umbral <- 0.10
 max_output <- 4
 confidence_threshold = .85
-min_row_by_style_size <- 8
-percent_remove_points <- 0
-
-styles <- c('2941','3086','3281','3282','3646','3954','852189','N6101','N730023','P011', 'P012','P013','P029')
+min_row_by_style_size <- 4
+percent_remove_points <- .0
 
 data.raw = read.csv("measurement_vectors.csv")
 
@@ -17,14 +15,15 @@ cmd <- "table(data.raw$style)"
 print(cmd)
 eval(parse(text=cmd))
 
-style <- 'P011'
+style <- 'P029'
 data.style = data.raw[data.raw$style == style,]
 
 data.style$size = sapply(data.style$size, function(x) make.names(x))
 data.trans <- eof(data.style[1:11])
 data.style[1:11] <- data.trans$A
 
-data.cooked = remove_outliers(data.style, c(1:11))
+#data.cooked = remove_outliers(data.style, c(1:11))
+data.cooked = data.style
 
 ctrl <- trainControl(method="cv", number=5, classProbs=TRUE, summaryFunction=customSummary)
 fit <- train(data.cooked[1:11], as.factor(data.cooked$size), tuneLength=5, method="multinom", maxit=500, trControl=ctrl)
